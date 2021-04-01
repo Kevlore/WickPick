924c25eb28e25c84331092c570d8d1dcab47974a
Logan <logalamo@gmail.com>
2021-03-31 20:12:19 -0500

    Set up active admin to use all the models

---
+++ app/admin/candles.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Candle do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :scent, :colour, :size, :price, :order_detail_id, :type_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:scent, :colour, :size, :price, :order_detail_id, :type_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_detail
-  validates :shipping_address, :date, :order_status presence: true
+  validates :shipping_address, :date, :order_status, presence: true
 end
