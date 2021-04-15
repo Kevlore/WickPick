2645bff036196d01e9497966fd57f0a9ec3aedd7
Logan <logalamo@gmail.com>
2021-04-15 13:04:13 -0500

    Added devise to customers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_174444) do
+ActiveRecord::Schema.define(version: 2021_04_15_174704) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -86,7 +86,14 @@ ActiveRecord::Schema.define(version: 2021_04_15_174444) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "provinces_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["provinces_id"], name: "index_customers_on_provinces_id"
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "order_details", force: :cascade do |t|
