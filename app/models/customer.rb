8e1249ae1195f87a0e5e6111bc5c6dd9a9314a20
Logan <logalamo@gmail.com>
2021-04-15 12:41:38 -0500

    Configured devise for login

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :username, :email, uniqueness: true
   validates :username, :password, :full_name, :email, :address, presence: true
   has_many :orders
