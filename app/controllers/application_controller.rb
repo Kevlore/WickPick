4531d2c0cc2e385b5e5182d6462ee4408d663b32
Logan <logalamo@gmail.com>
2021-04-15 14:30:56 -0500

    Revising the registrations controller for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,4 @@ class ApplicationController < ActionController::Base
     # Takes in an array of id's and returns a collection of candles
     Candle.find(session[:shopping_cart])
   end
-
-
-  protected
-  def
-    configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :full_name, :address, :province, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :full_name, :address, :province, :password, :current_password) }
-    end
 end
