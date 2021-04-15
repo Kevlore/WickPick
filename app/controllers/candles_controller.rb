522da4eb987bc13020d9d8c3780bfa62f81e52f4
Logan <logalamo@gmail.com>
2021-04-14 20:02:36 -0500

    Fixed an issue preventing the search bar category functionality.

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -1,12 +1,12 @@
 class CandlesController < ApplicationController
   def index
     # @candles = Candle.page params[:page]
-    if params['type'].blank? or params['type']['id'].blank?
+    if params['type'].blank? or params['type']['type_id'].blank?
       @candles = Candle.all
     else
-      # type = Type.find(params['type']['id'])
+      # type = Type.find(params['type']['type_id'])
       # @candles = type.candles
-      @candles = Type.find(params['type']['id']).candle
+      @candles = Type.find(params['type']['type_id']).candle
     end
 
     @candles = @candles.search(params[:keywords]).order('created_at DESC')
