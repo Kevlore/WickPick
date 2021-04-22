35b742c7d281f4d70fd3e2939ee56df22328a93a
Logan <logalamo@gmail.com>
2021-04-21 23:24:20 -0500

    Fixed an issue preventing orders from saving to the database.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,12 +16,44 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new()
+    # @order = Order.new(order_params)
+    if current_customer
+      @customer = current_customer
+    else
+      redirect_to candles_path
+    end
 
-    if order.save
-      redirect_to @order
+    province = Province.find(@customer.provinces_id)
+    pst = province.pst
+    gst = province.gst
+    hst = province.hst
+
+    customer_id = Customer.find (@customer.id)
+    new_order = Order.create(
+      shipping_address: @customer.address,
+      date: Date.today.to_s,
+      order_status: "Ordered",
+      pst: pst,
+      gst: gst,
+      hst: hst,
+      customer: customer_id
+    )
+
+    if new_order.valid?
+      redirect_to "/candles"
     else
       render :new
     end
+
+    # if @order.save
+    #   redirect_to @order
+    # else
+    #   render :new
+    # end
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:shipping_address, :date, :order_status, :pst, :gst, :hst, :customer, :price)
+    end
 end
